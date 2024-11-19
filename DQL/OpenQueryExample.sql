d19c531f3fd9cf1debeb23fdb89d195b101cc6a4
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-11-19 11:53:58 -0500

    Delete DQL directory

--- DQL/OpenQueryExample.sql
+++
@@ -1,6 +0,0 @@
-/* Problem: New production line has been added that has an oracle database where values are updated. 
-/* Solution: Use linked server in SQL Server and write an OPENQUERY stored procedure to move data to local table.
-/* Author: Michael Kalkas
-/* Date: 10/6/2020
-*/
-
