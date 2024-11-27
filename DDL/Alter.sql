3895df7772469cd435e746e89c9eba400821f3d0
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-11-27 17:27:41 -0500

    Update Alter script
    Updated file to contain real ALTER script on a function

--- DDL/Alter.sql
+++ DDL/Alter.sql
@@ -1,5 +1,24 @@
-/*
-* problem: Show how to alter a SP
-* Thoughts: Show why SP needs altered and how to do so
-* Date:
-*/
+USE [Manufacturing_DCS]
+GO
+/****** Object:  UserDefinedFunction [dbo].[ufnSQLCLEAN]    Script Date: 7/2/2021 2:49:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*************************************************************************************************************************
+* Author: Michael Kalkas
+* Date: 07/02/2021
+* Description: Created this function to clean varchars to ensure no unintentional query injection through the Varchar
+*
+**************************************************************************************************************************/
+ALTER Function [dbo].[ufnSQLCLEAN] (@strText VARCHAR(1000))
+RETURNS VARCHAR(1000)
+AS
+BEGIN
+	WHILE PATINDEX('%[^0-9,a-z,'' '',.,-]%', @strText) > 0
+	BEGIN
+		SET @strText = STUFF(@strText, PATINDEX('%[^0-9,a-z,'' '',.,-]%', @strText), 1, '')
+	END
+	RETURN @strText
+END
