ac57b48a37ed1a843a8a9773e26755867449ae46
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-12-03 12:14:34 -0500

    Update Grant with Example code

--- DCL/Grant.sql
+++ DCL/Grant.sql
@@ -1,5 +1,20 @@
-/*
-* problem: Show process for giving user rights on login
-* Thoughts: Show why user rights are given this way
-* Date:
-*/
+/****************************************************************************************
+* Author: Michael Kalkas
+* Date: 5/16/2022
+* Description: This script adds the Login, User and permissions for the Windows service
+*              I am building that sends member data for assignment to NCFast.
+****************************************************************************************/
+USE PartnersDev
+GO
+
+CREATE LOGIN <domainName>\WindowsServiceTransmit FROM WINDOWS;
+GO
+
+CREATE USER WindowsServiceTransmit FROM <domainName>\WindowsServiceTransmit
+Go
+
+GRANT EXECUTE ON TP_Member834_ToSend TO ServiceTransmit WITH GRANT OPTION;
+EXEC sp_addrolemember ServiceTransmit, WindowsServiceTransmit;
+
+
+Go
