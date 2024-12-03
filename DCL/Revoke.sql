cfe4afc5fa26d435ef362e93744067e016b98912
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-12-03 12:53:44 -0500

    Update Revoke with sample code

--- DCL/Revoke.sql
+++ DCL/Revoke.sql
@@ -1,5 +1,16 @@
-/*
-* problem: Explain scenario of user leaving company
-* Thoughts: Show how to remove rights of user in DB/Server
-* Date:
-*/
+/****************************************************************************************
+* Author: Michael Kalkas
+* Date: 5/18/2022
+* Description: This script removes the User and permissions for an old Windows service
+*              that was previously attempted by a developer and drops an old user.
+****************************************************************************************/
+
+USE PartnersDev
+GO
+
+REVOKE ALL FROM DWestbrookTestService;
+
+GO
+
+DROP USER IF EXISTS TMartin;
+GO
