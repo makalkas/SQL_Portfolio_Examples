f92493712bf8f82044911dbe95f0a7cf75ade81f
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-12-03 15:38:06 -0500

    Update Delete with example script

--- DML/Delete.sql
+++ DML/Delete.sql
@@ -1,5 +1,13 @@
-/*
-* Problem: Show how to delete based on filter conditions
-* Thoughts: Show proper way to delete records based on filters
-* Date:
-/*
+/************************************************************************************
+* AUTHOR: Michael Kalkas
+* DATE: 10/16/2021
+* DESCRIPTION: During testing current process is sending all records to NCFast. This
+*              means we need to delete anything that has been sent previously.
+*
+************************************************************************************/
+
+USE PartnersDev
+GO
+
+DELETE FROM TP_Members834_ToSend WHERE SentToNCFast = 1;
+GO
