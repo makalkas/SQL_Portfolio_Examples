47c1da68b450dc5599c03e610506a163f9444f15
MICHAEL KALKAS <71030672+makalkas@users.noreply.github.com>
2024-12-03 13:11:18 -0500

    Update Drop with example code

--- DDL/Drop.sql
+++ DDL/Drop.sql
@@ -1,5 +1,25 @@
-/*
-* problem: Show why dropping table is necessry
-* Thoughts: Show how to drop table appropriately
-* Date:
-*/
+/**********************************************************************************
+* Author: Michael Kalkas
+* DATE: 2/20/21
+* Description: This script was used to reset tables & procedures during development when
+*              new columns, keys, indexes needed to be added to system and/or requirement
+*              changes required compleate procedure changes.
+**********************************************************************************/
+
+USE PartnersDev
+GO
+
+DROP TABLE IF EXISTS dbo.TP_Member834_ToSend;
+DROP TABLE IF EXISTS dbo.TP_MemberPossibleTP_PCPs;
+DROP TABLE IF EXISTS dbo.TP_MemberSentLog;
+DROP TABLE IF EXISTS dbo.TP_MemberTPPCP;
+GO
+
+DROP PROCEDURE IF EXISTS
+	dbo.TP_GetNewMembers,
+	dbo.TP_DetermineCareTaker,
+	dbo.TP_DeterminePriorityServices,
+	dbo.TP_DetermineFamilyMemberAssociations,
+	dbo.TP_DeterminePossibleTP_PCP,
+	dbo.TP_AssignTP_PCP;
+GO
